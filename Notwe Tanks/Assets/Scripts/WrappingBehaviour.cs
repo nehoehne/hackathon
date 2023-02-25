90dc0b8476475e10dbf7b4268dcac7961c3f4a6b
nehoehne <77899522+nehoehne@users.noreply.github.com>
2023-02-25 17:41:50 -0600

    explosion animation

--- Notwe Tanks/Assets/Scripts/WrappingBehaviour.cs
+++ Notwe Tanks/Assets/Scripts/WrappingBehaviour.cs
@@ -13,27 +13,22 @@ public class WrappingBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		Debug.Log("YO");
 		Vector3 pos = transform.position;
 		if (transform.position.x > 80)
 		{
 			pos.x = -79;
-			Debug.Log("Right");
 		}
 		else if (transform.position.x < -80)
 		{
 			pos.x = 79;
-			Debug.Log("Left");
 		}
 		else if (transform.position.y > 45)
 		{
 			pos.y = -44;
-			Debug.Log("Top");
 		}
 		else if (transform.position.y < -45)
 		{
 			pos.y = 44;
-			Debug.Log("Bottom");
 		}
 		transform.position = pos;
 	}
