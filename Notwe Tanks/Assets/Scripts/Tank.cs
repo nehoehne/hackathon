7ec9611bac19c8d43b5a17562430ebe80ca30c9d
Kalen Arendt <dev.KalenArendt@Gmail.com>
2023-02-25 11:04:48 -0600

    initialized some scene objects and classes

---
+++ Notwe Tanks/Assets/Scripts/Tank.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using Assets.Scripts;
+
+using UnityEngine;
+
+public class Tank : MonoBehaviour
+{
+	[SerializeField] PlayerEnum playerEnum;
+	[SerializeField] Bullet bulletPrefab;
+
+	int numBullets = 3;
+
+	// Start is called before the first frame update
+	void Start ()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+
+}
