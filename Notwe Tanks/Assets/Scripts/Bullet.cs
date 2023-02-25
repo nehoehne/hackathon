45846ced1a450ec25e0be9ba3cdbdcfdd9104737
Nolan <nolanplett1000@gmail.com>
2023-02-25 12:55:11 -0600

    bullet stuff

--- Notwe Tanks/Assets/Scripts/Bullet.cs
+++ Notwe Tanks/Assets/Scripts/Bullet.cs
@@ -7,15 +7,28 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+	[SerializeField] float speed = 20;
+	public int thing;
+	private Rigidbody2D myBody;
+
 	public PlayerEnum ShotBy { get; set; }
 
 	public delegate void BulletEvent (PlayerEnum playerEnum);
 	public static event BulletEvent OnBulletDestroyed;
 
+
+	void Awake()
+	{
+		//called once
+
+		myBody = gameObject.GetComponent<Rigidbody2D>();
+
+	}
+
 	// Start is called before the first frame update
 	void Start()
     {
-        
+        Destroy(gameObject, 5f);
     }
 
     // Update is called once per frame
@@ -24,15 +37,28 @@ public class Bullet : MonoBehaviour
         
     }
 
+	public void InitBullet(Tank tank)
+	{
+		//Rigidbody2D tankBody = tank.GetComponent<Rigidbody2D>();
+
+		// set position and rotation to same as tank that shot it
+		//myBody.MovePosition(tankBody.position);
+		//myBody.MoveRotation(tankBody.rotation);
 
 
-
+		myBody.velocity = tank.transform.up * speed;
+		Debug.Log(transform.forward);
+		ShotBy = tank.GetPlayer;
+	}
 
 
 	private void OnTriggerEnter2D (Collider2D collision)
 	{
 		if (OnBulletDestroyed != null) {
 			OnBulletDestroyed(ShotBy);
+			// destory this gameojbect
+			//Destroy(gameObject);
+			Destroy(gameObject);
 		}
 	}
 }
