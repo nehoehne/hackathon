a47cf7f4fca369cfda21095d1f066f0e077b7d7a
Kalen Arendt <dev.KalenArendt@Gmail.com>
2023-02-25 19:29:15 -0600

    Dead Level and LevelManager

--- Notwe Tanks/Assets/Scripts/LevelManager.cs
+++ Notwe Tanks/Assets/Scripts/LevelManager.cs
@@ -4,51 +4,92 @@ using UnityEngine.SceneManagement;
 
 class LevelManager : MonoBehaviour
 {
-	//[SerializeField] float sceneLoadDelay = 1;
+	[SerializeField] float sceneLoadDelay = 1;
+
+
+	const string
+		SETTINGS_MENU = "Settings Menu",
+		MAIN_MENU = "Main Menu",
+		GAME_SCENE = "Game Scene",
+		GAME_OVER = "Game Over";
+
+
+	public enum SceneEnum
+	{
+		MainMenu,
+		Settings,
+		Game,
+		GameOver
+	}
+
+	public string SceneEnumToString(SceneEnum scene)
+	{
+		switch (scene) {
+			case SceneEnum.MainMenu: return MAIN_MENU;
+			case SceneEnum.Settings: return SETTINGS_MENU;
+			case SceneEnum.Game: return GAME_SCENE;
+			case SceneEnum.GameOver: return GAME_OVER;
+			default: return MAIN_MENU;
+		}
+	}
 
 
 	//public static LevelManager Instance { get; private set; } = null;
 
-	//void Awake()
+	//void Awake ()
 	//{
-	//	if( Instance == null )
-	//	{
+	//	if( Instance == null ) {
 	//		Instance = this;
 	//		DontDestroyOnLoad(gameObject);
 	//	}
-	//	else
-	//	{
+	//	else {
 	//		gameObject.SetActive(false);
 	//		DestroyImmediate(gameObject);
 	//	}
 	//}
 
-
-	//public void LoadMainMenu()
+	//public void LoadLevel (SceneEnum scene)
 	//{
-	//	SceneManager.LoadScene("0 Main Menu");
+	//	SceneManager.LoadScene(SceneEnumToString(scene));
 	//}
 
-	//public void LoadGame()
+	//public void LoadLevel(string levelName)
 	//{
-	//	ScoreKeeper.ResetScore();
-	//	SceneManager.LoadScene("1 Game");
+	//	SceneManager.LoadScene(levelName);
 	//}
 
-	//public void LoadGameOver()
-	//{
-	//	StartCoroutine(WaitAndLoadLevel("2 Game Over", sceneLoadDelay));
-	//}
 
-	//public void QuitGame()
-	//{
-	//	Debug.Log("Quiting Game...");
-	//	Application.Quit();
-	//}
 
-	//public IEnumerator WaitAndLoadLevel(string sceneName, float delay)
-	//{
-	//	yield return new WaitForSeconds(delay);
-	//	SceneManager.LoadScene(sceneName);
-	//}
+
+	public void LoadMainMenu ()
+	{
+		SceneManager.LoadScene("Main Menu");
+	}
+
+	public void LoadSettingsMenu ()
+	{
+		SceneManager.LoadScene("Settings Menu");
+	}
+
+	public void LoadGame ()
+	{
+		SceneManager.LoadScene("Game Scene");
+	}
+
+	public void LoadGameOver ()
+	{
+		StartCoroutine(WaitAndLoadLevel("Game Over", sceneLoadDelay));
+	}
+
+	public void QuitGame ()
+	{
+		Debug.Log("Quiting Game...");
+		Application.Quit();
+	}
+
+	public IEnumerator WaitAndLoadLevel (string sceneName, float delay)
+	{
+		yield return new WaitForSeconds(delay);
+		SceneManager.LoadScene(sceneName);
+	}
 }
